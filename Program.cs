1999ffce1d9821cc21213e970acfd61a25900931
fffff1 <yanasadovnikova68@gmail.con>
2024-05-04 10:33:34 +0400

    +

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace pract3lit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int zn;
+            Console.WriteLine("введите число а:");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("введите число б:");
+            b = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("введите знак:");
+            zn = Convert.ToChar(Console.ReadLine());
+            if(zn == '+')
+            {
+                int c = a+ b;   
+            Console.WriteLine("итог: "+c);
+            }
+            
+        }
+    }
+}
